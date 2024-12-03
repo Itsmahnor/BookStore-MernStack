2e96f72e69c805d5677a47389aa342e9f9da6315
Mahnoor Sarfraz <mahnoorsarfraz23@gmail.com>
2024-12-03 19:48:11 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,35 @@
-import express from 'express'
-import  {PORT}  from './config.js';
-import mongoose from 'mongoose';
-import { Book } from './Model/model.js';
-import cors from 'cors';
-import router from './Router/router.js';
- const app=express();
- app.use(express.json());
- app.use(express.urlencoded({extended:false}));
- app.use(cors({
-    origin: ["https://book-store-frontend-535wrv0v4-mahnoors-projects-9520722d.vercel.app/"], // Allowed origin
+import express from "express";
+import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./Model/model.js";
+import cors from "cors";
+import router from "./Router/router.js";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "https://book-store-frontend-535wrv0v4-mahnoors-projects-9520722d.vercel.app", // Fixed origin without trailing slash
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Allowed HTTP methods
-    credentials: true // Enable credentials for cookies
-}));
+    credentials: true, // Enable credentials for cookies
+  })
+);
 
+// Routes
+app.use("/books", router);
 
-app.use("/books",router)
- mongoose.connect("mongodb+srv://mongo:mongo@cluster0.sg3gm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
-  console.log("Server Connected");
-  app.listen(PORT,()=>{
-    console.log(`App is listening to port ${PORT}`);
-   })
- }).catch((err)=> console.log(err.message));
-
+// MongoDB Connection
+mongoose
+  .connect(
+    "mongodb+srv://mongo:mongo@cluster0.sg3gm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Server Connected");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err.message));
