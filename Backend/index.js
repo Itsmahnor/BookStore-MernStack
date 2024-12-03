9b15ead8e207e2a483fa8509d657327d786e5814
Mahnoor Sarfraz <mahnoorsarfraz23@gmail.com>
2024-12-03 20:55:33 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,20 @@
 import express from "express";
 import { PORT } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./Model/model.js";
 import cors from "cors";
 import router from "./Router/router.js";
 
 const app = express();
 
 // Middleware
+app.use(cors({
+    origin: "https://book-store-frontend-flax.vercel.app",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-app.use(cors({
-    origin: "https://book-store-frontend-flax.vercel.app", // Your frontend's URL
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Allowed HTTP methods
-    credentials: true // Allow cookies or authorization headers
-}));
-
-app.use(express.json());
 // Routes
 app.use("/books", router);
 
